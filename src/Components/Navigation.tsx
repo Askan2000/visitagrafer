55b7624213bc7806cb379a0dce25ba2c8e4a2828
Oscar Scheja <scheja.oscar@gmail.com>
2023-05-11 14:22:53 +0200

    Ändrade om syntax för länkningen i Navigation

--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavDropdown, Navbar } from 'react-bootstrap';
-import { Link, Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import FetchDataMonthlyKPI from './FetchData/FetchDataMonthlyChange';
 import FetchDataYearlyKPI from './FetchData/FetchDataYearlyChange';
 import FetchDataYearlyChangeTotAndRest from './FetchData/FetchDataYearlyChangeTotAndRest'
@@ -17,23 +17,23 @@ const Navigation: React.FC = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <NavDropdown data-cy="dropdown" title="Välj graf" id="basic-nav-dropdown">
-          <NavDropdown.Item href="/visitagrafer"> Hem </NavDropdown.Item>
-          <NavDropdown.Item data-cy="dropdown-item2" href="/kpimonthlyrestaurants">
+          <NavDropdown.Item  to="/visitagrafer" as={Link}> Hem </NavDropdown.Item>
+          <NavDropdown.Item data-cy="dropdown-item2" to="/kpimonthlyrestaurants" as={Link}>
             Restaurangpriser månadstakt
           </NavDropdown.Item>
-          <NavDropdown.Item href="/kpiyearlyrestaurants">
+          <NavDropdown.Item to="/kpiyearlyrestaurants" as={Link}>
             Restaurangpriser årstakt
           </NavDropdown.Item>
-          <NavDropdown.Item href="/kpirestaurantsandtotal">
+          <NavDropdown.Item to="/kpirestaurantsandtotal" as={Link}>
             KPI och restaurangpriserna
           </NavDropdown.Item>
-          <NavDropdown.Item href="/spi">
+          <NavDropdown.Item to="/spi" as={Link}>
             SPI per produktkategori
           </NavDropdown.Item>
-          <NavDropdown.Item href="/kpiandspi">
+          <NavDropdown.Item to="/kpiandspi" as={Link}>
             KPI och SPI
           </NavDropdown.Item>
-          <NavDropdown.Item data-cy="dropdown-item6" href="/eurostat">
+          <NavDropdown.Item data-cy="dropdown-item6" to="/eurostat" as={Link}>
             Prisutveckling restaurang norden
           </NavDropdown.Item>
         </NavDropdown>
